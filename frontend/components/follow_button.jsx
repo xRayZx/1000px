7c15b4877ced013d5dc2e4664f05998cc49fa142
Raymond Zhang <zhang.rayz@gmail.com>
2016-07-07 11:11:45 -0700

    super dynamic follow buttons

--- frontend/components/follow_button.jsx
+++ frontend/components/follow_button.jsx
@@ -2,6 +2,11 @@ const React = require('react');
 const FollowAction = require('../actions/follow_actions.js');
 
 const Follow = React.createClass({
+	getInitialState () {
+		return ({
+			hover: false
+		});
+	},
 	_toggleFollow () {
 		if (this.props.following) {
 			FollowAction.unfollow(this.props.user);
@@ -12,17 +17,32 @@ const Follow = React.createClass({
 			this.props.updateButton(this.props.user);
 		}
 	},
+	_mouseOver () {
+		this.setState({hover: true});
+	},
+	_mouseOut () {
+		this.setState({hover: false});
+	},
 	render () {
 		let style = "follow-btn btn btn-primary";
 		if (this.props.from === "suggest") {
 			style = "suggest-follow-btn";
 		}
 		let value = "Follow";
-		if (this.props.following) {
+		if (this.props.following && this.props.from === "suggest" && this.state.hover) {
+			value = "Unfollow";
+			style = "suggest-follow-btn red"
+		} else if (this.props.following && this.state.hover) {
+			value = "Unfollow";
+			style = "follow-btn btn btn-danger";
+		} else if (this.props.following && this.props.from === "suggest") {
+			value = "Following";
+		} else if (this.props.following) {
+			style = "follow-btn btn btn-success";
 			value = "Following";
 		}
 		return (
-			<button onClick={this._toggleFollow} className={style}>{value}</button>
+			<button onClick={this._toggleFollow} onMouseOver={this._mouseOver} onMouseOut={this._mouseOut} className={style}>{value}</button>
 		)
 	}
 });
