e6d354bd0e3173dd3b3e490c1ebcb4f57fa365a2
Raymond Zhang <zhang.rayz@gmail.com>
2016-07-06 21:47:09 -0700

    comments can link to poster's profile page

--- frontend/components/comments.jsx
+++ frontend/components/comments.jsx
@@ -1,4 +1,5 @@
 const React = require('react');
+const hashHistory = require('react-router').hashHistory;
 const CommentStore = require('../stores/comment_store.js');
 const CommentActions = require('../actions/comment_actions.js');
 const CloudinaryUtil = require('../util/cloudinary_util.js');
@@ -19,21 +20,26 @@ const Comments = React.createClass({
 	_onChange() {
 		this.setState({comments: CommentStore.all()});
 	},
+	showProfile (userId) {
+		return (e) => {
+			hashHistory.push(`/profile/${userId}`);
+		}
+	},
 	render () {
 		let comments = [];
 		this.state.comments.forEach( (comment) => {
 			let element = (
 				<li className="comment" key={comment.id}>
 					<div className="commentor-img">
-						<img src={CloudinaryUtil.image(comment.poster_pic, {width: 35, gravity: 'face', crop: 'thumb'})}/>
+						<img onClick={this.showProfile(comment.poster_id)} src={CloudinaryUtil.image(comment.poster_pic, {width: 35, gravity: 'face', crop: 'thumb'})}/>
 					</div>
 					<div className="comment-text">
-						<span><strong>{comment.poster} </strong></span>
+						<span><strong onClick={this.showProfile(comment.poster_id)}>{comment.poster} </strong></span>
 						<div>{comment.body}</div>
 					</div>
 				</li>
 			);
-			comments.push(element);
+			comments.unshift(element);
 		} )
 		return (
 			<div className="comments-container">
