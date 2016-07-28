4314fcdbbee37caeb899e07ac10bf1e1197ea251
Raymond Zhang <zhang.rayz@gmail.com>
2016-07-27 20:56:20 -0700

    sign up requires first and last name. new users has default profile pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_reader :password
 
-  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :password_digest, :session_token, :first_name, :last_name, presence: true
   validates :username, uniqueness: true
   validates :password, length: {minimum: 6, allow_nil: true}
 
